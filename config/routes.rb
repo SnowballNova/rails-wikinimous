c12bc5403f70a7d4f2ddcd915fcf0367c1f062df
Charles Boucher <charlesb.ipag@gmail.com>
2024-11-12 15:41:26 +0100

    Articles Management

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,17 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get '/articles', to: 'articles#index'
 
+  get    "articles/new",      to: "articles#new",  as: :new_article
+  post   "articles",          to: "articles#create"
+
+  get "articles/:id/edit", to: "articles#edit", as: :edit_article
+  patch "articles/:id", to: "articles#update"
+
+  delete "articles/:id", to: "articles#destroy"
+
+  get "articles/:id", to: "articles#show", as: :article
   # Defines the root path route ("/")
   # root "posts#index"
 end
